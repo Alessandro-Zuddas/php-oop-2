a44131521a09c1273e1a3a5dc6c4a74bbaa6cf8a
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-01-11 15:07:26 +0100

    Aggiunge gestione errore costruzione categoria

--- classes/Category.php
+++ classes/Category.php
@@ -6,7 +6,11 @@ class Category{
 
     public function __construct(string $_type)
     {
-        $this->type = $_type;
+        if($_type != "dog" && $_type != "cat"){
+            throw new Exception("Puoi inserire solo una stringa!");
+        }else{
+            $this->type = $_type;
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require_once __DIR__ . "/classes/products/Food.php";
 require_once __DIR__ . "/classes/products/Kennel.php";
 require_once __DIR__ . "/classes/products/Toy.php";
 
-$category_dog = new Category("dog");
-$category_cat = new Category("cat");
+try{
+    $category_dog = new Category("dog");
+    $category_cat = new Category("cat");
+}
+catch(Exception $e)
+{
+    echo $e;
+}
 
 $prova_food_dog = new Food("Prova titolo", "Prova descrizione", 10.50, $category_dog, "Prova ingredienti", "Prova dosaggio");
 $prova_food_cat = new Food("Prova titolo", "Prova descrizione", 10.50, $category_cat, "Prova ingredienti", "Prova dosaggio");
